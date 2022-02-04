5e8688f1b4cae4dcee793d5ebd074c7fc2e47189
Chad Tarpey <chadtarpey@gmail.com>
2022-02-04 15:57:40 -0600

    moved all user routes method to user controller

---
+++ controllers/userController.js
@@ -0,0 +1,63 @@
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const config = require("config")
+const User = require("../models/User")
+
+exports.createUser = async (req, res) => {
+    console.log("User Routes hitt")
+    const { username, email, password } = req.body;
+
+    try
+    {
+        let user = await User.findOne({ email: email })
+
+        if (user)
+        {
+            return res.status(400).json({ msg: "User Already Exist" })
+        }
+
+        user = new User({
+            username,
+            email,
+            password
+        })
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt)
+
+        // return json token 
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get("jwtSecret"),
+
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token })
+            }
+        )
+
+        user.save()
+
+
+    } catch (error)
+    {
+        console.log(error.message);
+        res.status(500).send("Server Error")
+    }
+}
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+
+    res.json({
+        _id: user._id,
+        username: user.username,
+        email: user.email
+    })
+}
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,68 +1,11 @@
-const express = require("express")
+const express = require("express");
+const { createUser, login } = require("../../controllers/userController");
 const router = express.Router();
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const config = require("config")
-const User = require("../../models/User")
-
-router.post("/", async (req, res) => {
-    console.log("User Routes hitt")
-    const { username, email, password } = req.body;
-
-    try
-    {
-        let user = await User.findOne({ email: email })
-
-        if (user)
-        {
-            return res.status(400).json({ msg: "User Already Exist" })
-        }
-
-        user = new User({
-            username,
-            email,
-            password
-        })
-
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt)
-
-        // return json token 
-        const payload = {
-            user: {
-                id: user.id
-            }
-        }
-
-        jwt.sign(payload, config.get("jwtSecret"),
-
-            { expiresIn: 360000 },
-            (err, token) => {
-                if (err) throw err;
-                res.json({ token })
-            }
-        )
-
-        user.save()
 
 
-    } catch (error)
-    {
-        console.log(error.message);
-        res.status(500).send("Server Error")
-    }
-})
+router.post("/", createUser)
 
 
-router.post("/login", async (req, res) => {
-    const { email, password } = req.body
-    const user = await User.findOne({ email })
-
-    res.json({
-        _id: user._id,
-        username: user.username,
-        email: user.email
-    })
-})
+router.post("/login", login)
 
 module.exports = router
