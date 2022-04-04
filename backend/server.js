9e3a658f279fcf801d1418b3fa30012487ef3d1b
Chad Tarpey <chadtarpey@gmail.com>
2022-04-04 16:49:25 -0500

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv").config()
 const app = express();
 const path = require("path");
 // const userRoutes = require("./routes/api/users")
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 
 // connect to database
