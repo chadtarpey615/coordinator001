64e0afb8b7a51bcdb3b7c7dfae1eecf915c01652
Chad Tarpey <chadtarpey@gmail.com>
2022-07-22 18:19:41 -0500

    working on new navbar with tailwind

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -53,89 +53,159 @@ const Navbars = () => {
 
     return (
         <>
-            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" fixed="top" className="py-1">
-                <div className="container-fluid">
-                    <Link className="navbar-brand text-white" to="/signup">Run-Fit</Link>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav" >
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 " >
-                            <li className="nav-item">
-                                <Link className="nav-link active text-white" aria-current="page" to="/">Home</Link>
+
+            <div className="flex flex-row items-center">
+                <div>
+                    <li className="mx-2">
+                        <Link className=" text-white" aria-current="page" to="/">Home</Link>
+                    </li>
+                </div>
+
+                <div>
+                    <li className="mx-2" onToggle >
+                        <Link className="text-white" to="/users" >Users</Link>
+                    </li>
+                </div>
+                <div>
+                    <li className="mx-2">
+                        <Link className="text-white" to="/calendar"  >Calendar</Link>
+                    </li>
+                </div>
+                <div>
+                    <li className="mx-2">
+                        <Link className="text-white" to="/events">All Events</Link>
+                    </li>
+                </div>
+            </div>
+            {/* <nav>
+                <div className="flex flex-row bg-slate-700 h-12 items-center">
+                    <Link className="text-white" to="/signup">Run-Fit</Link>
+                    <div className="basis-1/3 ">
+                        <ul className="mx-2" >
+                            <li className="mx-2">
+                                <Link className="active text-white" aria-current="page" to="/">Home</Link>
                             </li>
-                            <li className="nav-item" onToggle >
-                                <Link className="nav-link text-white" to="/users" >Users</Link>
+                            <li className="mx-2" onToggle >
+                                <Link className="text-white" to="/users" >Users</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link text-white" to="/calendar"  >Calendar</Link>
+                            <li className="mx-2">
+                                <Link className="text-white" to="/calendar"  >Calendar</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link text-white" to="/events">All Events</Link>
+                            <li className="mx-2">
+                                <Link className="text-white" to="/events">All Events</Link>
                             </li>
                         </ul>
-                        <NavDropdown.Divider bg="light" variant="light" />
+                    </div>
 
-                        {user ? (
-                            <ul className="navbar-nav ">
-                                <li className="nav-item">
-                                    <li className="nav-link active text-white" aria-current="page"><EmojiPeopleIcon /> {user.username} </li>
+                    {user ? (
+                        <ul className="basis-1/3">
+                            <li className="mx-2">
+                                <li className="text-white" aria-current="page"><EmojiPeopleIcon /> {user.username} </li>
+                            </li>
+                            <li className="mx-2">
+                                <Link onClick={userLogout} className="text-white" aria-current="page" to="/login">Log Out</Link>
+                            </li>
+                        </ul>
+                    ) : (
+
+                            <ul className="">
+                                <li className="mx-2">
+                                    <Link className="text-white" aria-current="page" to="/signup">Sign Up</Link>
                                 </li>
-                                <li className="nav-item">
-                                    <Link onClick={userLogout} className="nav-link active text-white" aria-current="page" to="/login">Log Out</Link>
+                                <li className="mx-2">
+                                    <Link className="text-white" aria-current="page" to="/login">Sign In</Link>
                                 </li>
                             </ul>
-                        ) : (
-
-                                <ul className="navbar-nav ">
-                                    <li className="nav-item">
-                                        <Link className="nav-link active text-white" aria-current="page" to="/signup">Sign Up</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link active text-white" aria-current="page" to="/login">Sign In</Link>
-                                    </li>
-                                </ul>
-                            )}
-
-                        {user ? (
-                            <>
-                                <li onClick={handleOpen} className="nav-link active text-white" aria-current="page" >Friends</li>
-                                <Modal
-                                    open={open}
-                                    onClose={handleClose}
-                                    aria-labelledby="modal-modal-title"
-                                    aria-describedby="modal-modal-description"
-                                >
-                                    <Box sx={style}>
-                                        <Stack spacing={2}>
-
-
-                                            <p className="text-info">Friends</p>
-                                            <hr />
-                                            {friends.map((friend) => (
-
-                                                <h6 className="text-white">{friend}</h6>
-
-                                            ))}
-
-                                        </Stack>
-                                    </Box>
-                                </Modal>
-                            </>
-                        ) : (
-                                <p>User has no friends</p>
-
-                            )}
-
-
-                    </Navbar.Collapse>
-
+                        )}
                 </div>
-            </Navbar>
+
+
+
+            </nav> */}
 
         </>
     )
 }
 
 export default Navbars
+
+    // < Navbar collapseOnSelect expand = "md" bg = "dark" variant = "dark" fixed = "top" className = "py-1" >
+    //     <div className="container-fluid">
+    //         <Link className="navbar-brand text-white" to="/signup">Run-Fit</Link>
+    //         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    //         <Navbar.Collapse id="responsive-navbar-nav" >
+    //             <ul className="navbar-nav me-auto mb-2 mb-lg-0 " >
+    //                 <li className="nav-item">
+    //                     <Link className="nav-link active text-white" aria-current="page" to="/">Home</Link>
+    //                 </li>
+    //                 <li className="nav-item" onToggle >
+    //                     <Link className="nav-link text-white" to="/users" >Users</Link>
+    //                 </li>
+    //                 <li className="nav-item">
+    //                     <Link className="nav-link text-white" to="/calendar"  >Calendar</Link>
+    //                 </li>
+    //                 <li className="nav-item">
+    //                     <Link className="nav-link text-white" to="/events">All Events</Link>
+    //                 </li>
+    //             </ul>
+    //             <NavDropdown.Divider bg="light" variant="light" />
+
+    //             {user ? (
+    //                 <ul className="navbar-nav ">
+    //                     <li className="nav-item">
+    //                         <li className="nav-link active text-white" aria-current="page"><EmojiPeopleIcon /> {user.username} </li>
+    //                     </li>
+    //                     <li className="nav-item">
+    //                         <Link onClick={userLogout} className="nav-link active text-white" aria-current="page" to="/login">Log Out</Link>
+    //                     </li>
+    //                 </ul>
+    //             ) : (
+
+    //                     <ul className="navbar-nav ">
+    //                         <li className="nav-item">
+    //                             <Link className="nav-link active text-white" aria-current="page" to="/signup">Sign Up</Link>
+    //                         </li>
+    //                         <li className="nav-item">
+    //                             <Link className="nav-link active text-white" aria-current="page" to="/login">Sign In</Link>
+    //                         </li>
+    //                     </ul>
+    //                 )}
+
+    //             {user ? (
+    //                 <>
+    //                     <li onClick={handleOpen} className="nav-link active text-white" aria-current="page" >Friends</li>
+    //                     <Modal
+    //                         open={open}
+    //                         onClose={handleClose}
+    //                         aria-labelledby="modal-modal-title"
+    //                         aria-describedby="modal-modal-description"
+    //                     >
+    //                         <Box sx={style}>
+    //                             <Stack spacing={2}>
+
+
+    //                                 <p className="text-info">Friends</p>
+    //                                 <hr />
+    //                                 {friends.map((friend) => (
+
+    //                                     <h6 className="text-white">{friend}</h6>
+
+    //                                 ))}
+
+    //                             </Stack>
+    //                         </Box>
+    //                     </Modal>
+    //                 </>
+    //             ) : (
+    //                     <p>User has no friends</p>
+
+    //                 )}
+
+
+    //         </Navbar.Collapse>
+
+    //     </div>
+    //         </Navbar >
 
 
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from "react-redux"
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
 // import reportWebVitals from './reportWebVitals';
